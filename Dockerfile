3ea1c5678fd316ac77ff404160126af9fa0eadb3
Rakesh Chowdhury <rc202402@gmail.com>
2020-08-04 17:06:40 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu
+#Heroku
+#USER root
+
+# Expose 8080
+# Heroku PORT env var
+ENV PORT 8080
+EXPOSE 8080
+
+WORKDIR /pangolin
+ENTRYPOINT bash /pangolin/start.sh
+
+
+
+RUN apt update
+RUN apt install -y iproute2 iptables net-tools dos2unix
+COPY pangolin /pangolin
+RUN chmod 777 /pangolin/main
+RUN dos2unix /pangolin/start.sh
---
+++ pangolin/start.sh
@@ -0,0 +1,62 @@
+SERVERIP=0.0.0.0
+SERVERPORT=8080
+TOKENS='["12345", "12345678", "pangolin", "password"]'
+ROLE=SERVER
+
+function start_server ()
+{
+	ip tuntap add dev tun0 mod tun
+	ip addr add 10.0.0.2/8 dev tun0
+	ip link set tun0 up
+	ip link set dev tun0 mtu 1400
+	ifc=`route -n | awk '{if($1=="0.0.0.0" && $8 != "tun0"){print $8; exit;}}'`
+	INTERFACE="$ifc"
+	ip=`ip addr show dev "$ifc" | awk '$1 == "inet" { sub("/.*", "", $2); print $2 }'`
+
+	SERVERIP=$ip
+	iptables -t nat -F
+	iptables -t nat -A POSTROUTING -o "$ifc" -j SNAT --to-source $ip
+	iptables -P FORWARD ACCEPT
+	iptables -A INPUT -p tcp --destination-port `expr $SERVERPORT + 1` -j DROP
+
+	cp -f configs/cfg_server.json.template configs/cfg_server.json
+	replace configs/cfg_server.json
+	./main -c configs/cfg_server.json 
+}
+
+function start_client ()
+{
+	ip tuntap add dev tun0 mod tun
+	ip addr add 10.0.0.22/8 dev tun0
+	ip link set tun0 up
+	ip link set dev tun0 mtu 1400
+	iptables -t nat -F
+	iptables -t nat -A POSTROUTING -o tun0 -j SNAT --to-source 10.0.0.22
+	iptables -P FORWARD ACCEPT
+	ifc=`route -n | awk '{if($1=="0.0.0.0" && $8 != "tun0"){print $8; exit;}}'`
+	INTERFACE="$ifc"
+	
+	gw=`route -n | awk '$1 == "0.0.0.0" {print $2}'`
+	route add $SERVERIP gw $gw
+	route add default gw 10.0.0.1
+	echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+	cp -f configs/cfg_client.json.template configs/cfg_client.json
+	replace configs/cfg_client.json
+	./main -c configs/cfg_client.json 
+}
+
+
+function replace ()
+{	
+	sed -i "s/{SERVERIP}/$SERVERIP/g" $1
+	sed -i "s/{SERVERPORT}/$SERVERPORT/g" $1
+	sed -i "s/{TOKENS}/$TOKENS/g" $1
+	sed -i "s/{INTERFACE}/$INTERFACE/g" $1
+}
+
+
+[[ "$ROLE" == "SERVER" ]] && start_server
+[[ "$ROLE" == "CLIENT" ]] && start_client
+
+echo "ERROR: pangolin exit"
